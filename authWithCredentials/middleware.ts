08d6d64f99d2c439fa9b7f598d7047b8a4b11e93
Ali <alidrl26@gmail.com>
2024-01-27 13:00:30 +0200

    first

--- authWithCredentials/middleware.ts
+++ authWithCredentials/middleware.ts
@@ -1,3 +1,12 @@
-export {default} from 'next-auth/middleware'
+import { NextRequest, NextResponse } from 'next/server';
+
+// export {default} from 'next-auth/middleware'
+
+
+export function middleware(req:NextRequest){
+     console.log('lee');
+
+     return NextResponse.next()
+}
 
 export const config = {matcher:['/dashboard']}
